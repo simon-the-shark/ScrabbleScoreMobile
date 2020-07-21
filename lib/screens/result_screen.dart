363c5c669ed6bdf4bed9065c4004ec559f80095b
simon-the-shark <simon_rekin@o2.pl>
2020-07-22 00:18:26 +0200

    redesigned result view

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,19 +1,32 @@
-import 'package:app/widgets/scrabble_rose.dart';
+import 'package:auto_orientation/auto_orientation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../helpers/scrabble_helper.dart';
 import '../providers/game.dart';
-import '../widgets/scrabble_net.dart';
-import '../widgets/user_result_tile.dart';
+import '../widgets/podium_box.dart';
+import '../widgets/user_widget.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
   static const routeName = "/results";
-  static const alignments = {
-    1: Alignment.topCenter,
-    2: Alignment.centerLeft,
-    3: Alignment.centerRight,
-    4: Alignment.bottomCenter,
-  };
+
+  @override
+  _ResultScreenState createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  @override
+  void initState() {
+    AutoOrientation.landscapeAutoMode();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    AutoOrientation.fullAutoMode();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final appBar = AppBar(
@@ -21,30 +34,46 @@ class ResultScreen extends StatelessWidget {
       leading: const CloseButton(),
     );
     var players = Provider.of<Game>(context).players;
+    var height = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).padding.top -
+        appBar.preferredSize.height;
+    var width = MediaQuery.of(context).size.width * 0.75;
+    var platform = <Widget>[
+      for (var i = 0; i < players.length; i++)
+        SizedBox(
+          width: width / 4,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              FittedBox(
+                child: Text(
+                  players[i].value,
+                  style: ScrabbleHelper.textStyle.copyWith(
+                    color: UserWidget.colors[players[i].key],
+                  ),
+                ),
+              ),
+              Text(
+                Provider.of<Game>(context).points[players[i].key].toString(),
+                style: ScrabbleHelper.textStyle.copyWith(
+                  color: UserWidget.colors[players[i].key],
+                  fontSize: 35,
+                ),
+              ),
+              PodiumBox(height, width, i + 1),
+            ],
+          ),
+        )
+    ];
     return Scaffold(
-      backgroundColor: Colors.grey[200],
       appBar: appBar,
-      body: Stack(
-        children: <Widget>[
-          Center(
-            child: ScrabbleNet(),
-          ),
-          Center(
-            child: ScrabbleRose(),
-          ),
-          Center(
-            child: ConstrainedBox(
-              constraints:
-                  BoxConstraints(maxHeight: MediaQuery.of(context).size.width),
-              child: Stack(children: <Widget>[
-                for (var i = 0; i < players.length; i++)
-                  Align(
-                      alignment: alignments[i + 1],
-                      child: UserResultTile(players[i], i + 1)),
-              ]),
-            ),
-          ),
-        ],
+      body: Align(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: platform..replaceRange(0, 2, [platform[1], platform[0]]),
+        ),
       ),
     );
   }
---
+++ lib/widgets/podium_box.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../helpers/scrabble_helper.dart';
+import 'my_custom_icons_icons.dart';
+
+class PodiumBox extends StatelessWidget {
+  PodiumBox(this.height, this.width, this.place);
+
+  final int place;
+  final double height;
+  final double width;
+
+  static const placeColors = {
+    1: Color.fromRGBO(214, 175, 54, 1),
+    2: Color.fromRGBO(167, 167, 173, 1),
+    3: Color.fromRGBO(167, 112, 68, 1),
+    4: ScrabbleHelper.DIRTY_WHITE,
+  };
+  static const placeHeightsFactors = {
+    1: 0.6,
+    2: 0.4,
+    3: 0.3,
+    4: 0.1,
+  };
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: placeHeightsFactors[place] * height,
+      width: width / 4,
+      child: Card(
+        color: ScrabbleHelper.DIRTY_WHITE,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: FittedBox(
+            child: place == 1
+                ? Icon(MyCustomIcons.trophy, color: placeColors[1])
+                : place == 2
+                    ? Icon(MyCustomIcons.medal, color: placeColors[2])
+                    : place == 3
+                        ? Icon(MyCustomIcons.medal, color: placeColors[3])
+                        : Container(),
+          ),
+        ),
+      ),
+    );
+  }
+}
